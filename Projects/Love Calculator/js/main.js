deb881c70b5855ff3bb048697089a6c76807a4be
T-Shirts Mine <88222733+tshirtsmine@users.noreply.github.com>
2021-10-03 21:58:48 +0600

    JavaScript Love Calculator

---
+++ Projects/Love Calculator/js/main.js
@@ -0,0 +1,22 @@
+// First 2 Person Name Store in Variable
+let yourName = prompt('What is your Name?');
+let partnerName = prompt('What is your partner name?');
+
+
+
+// Random Number Generate & Show Percentage
+let randomNumber = Math.random() * 100;
+let showNumber = Math.floor(randomNumber) + 1;
+
+
+
+// Showing Love Percentage Result
+alert(yourName + " and "  + partnerName + "'s love percentage is: " + showNumber + " % ");
+
+
+
+
+
+
+
+
