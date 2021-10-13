54a9fb1aced4b80c86cd7d108a0d2f4829871a3b
Masudha Meher <55777560+samflab@users.noreply.github.com>
2021-10-13 10:11:26 +0530

    Create app.js

---
+++ Projects/random-lorem-ipsum/app.js
@@ -0,0 +1,47 @@
+const arr = ["Halm heescht un wee. Gei ze wuel rout d'Loft. Dem Blieder schaddreg am, vu Welt laacht schaddreg ech. All da rout riede Hémecht, wee ke iweral Stieren. Fond weisen ze rou, main bléit vu net.",
+    "Rei Kënnt gebotzt vu, wou drun bléit zwëschen vu, no Mamm frësch dén. Ké dan Gart Margréitchen, rou Halm eise welle an. As sin riede Wisen. Rei Schied Hémecht da, vu Mier Mamm uechter get.",
+    "Mat haut engem d'Sonn vu, schléit prächteg Milliounen un gét, hale d'Loft d'Natur un get. Ech un duerch jeitzt derfir. Rem jo soubal d'Leit, wär dé frou gesiess Keppchen, ké keng d'Margréitchen vun. Wéi jo Ierd Milliounen, jo méi d'Land d'Kirmes.",
+    "Wuel aremt hirem rei no, dem dé durch d'Wéën. Gëtt aremt as rei. Sin ké Ierd durch Gaart, gei ze ma'n koum Säiten. Op Kaffi erwaacht sou, um méi uechter d'Vioule Gesträich, méi um huet Monn Keppchen. As ons iech engem räich, Stad ménger gefällt wa hir. Op vun Biereg d'Pied Hämmel, ké ech alle dénen d'Kirmes.",
+    "पहोच। पढाए गटकउसि सोफ़तवेर क्षमता हमेहो। बनाना संभव भाषाओ क्षमता अधिकार विज्ञान रचना काम उपेक्ष कम्प्युटर पुर्व हमारी एसलिये व्याख्यान बदले बनाति हीकम संस्था पहोचने पासपाई बीसबतेबोध कोहम समजते कर्य जिसकी प्रेरना निर्माण वार्तालाप हुएआदि स्वतंत्रता नवंबर गटको",
+    "ध्वनि सुना ब्रौशर मानसिक विभाजन समूह विश्वास विश्वव्यापि विस्तरणक्षमता लक्षण लगती सदस्य वार्तालाप अर्थपुर्ण लक्षण एवम् क्षमता विकेन्द्रित गएआप मुख्य यायेका संपुर्ण लचकनहि वर्तमान पहोचाना हिंदी एछित विवरण उपेक्ष वेबजाल विकासक्षमता कैसे लेने समजते अनुकूल आवश्यकत करके(विशेष करके(विशेष मार्गदर्शन बाटते थातक पढाए सभीकुछ परिभाषित सुचना सक्षम जिम्मे तकनिकल प्राथमिक सहित उदेश नयेलिए अनुवादक लेकिन",
+    "भाषाओ परस्पर वर्तमान जाता किके हिंदी विनिमय अंतर्गत यन्त्रालय विकास भाषा अनुकूल विज्ञान सार्वजनिक मुश्किल क्षमता। हार्डवेर विकास कीसे औषधिक खयालात विनिमय सम्पर्क संभव खरिदे जैसी कुशलता तकनिकल आपको होसके प्रमान प्राथमिक कार्यसिधान्तो संदेश अथवा दुनिया करेसाथ है।अभी उद्योग आवश्यकत करके(विशेष विश्व सकता वर्णन आजपर दिशामे बेंगलूर अर्थपुर्ण विवरण किया क्षमता। वार्तालाप",
+    "Ja dev vnov zxen, gde esxte volapukio li, gaz ku pikant imajsx problem. Om tut hvala pomocit. Okno ovocx zemla bez ti, vo zemla pervju svekrbrat kak. Dengis divajm ona to, ja blizuo divajm zxoltju dev. Ozeros dobrju tof om, zavoduf voprosil vi des.",
+    "Des tenis verijm imenijt vo, ne mai oliv zubis. Iz oni varenie ostatju posteluf, om mozx bruho hrvatzem bez, no tot varenie odevijm priatel. Iz piat komnat pokazit bil. Vi dla gvoril videnies probudijm, okno gotovijut umivalnaf zxe do.",
+    "Ku nams varijm navestib tri. Mama pesok so din, tozx domo blizuo mai bi. Mozxuo domovijm vse li, zxe so idijte znajsx svadit. Sport svadit tut om. Den profesor umivalnaf to, insxto pomocijt prinesijsx moi es. Sos bu detes tancit. Dajt priatel cxesajut dla do, insxto ucxenie zlatoju tri do.",
+    "Na idijm vseslavju tof. Oliv silaju simbolijut ne moi. Ku lednek vsesxto zxe, ulca domo lovit zxe bi. Vasx kuharju mezxunarodju bo sam, on bil divaj nadeam, pocx cxehzem bu sos. Te gde dumajm priatelica, so ona vasx iskajm. Novuo takak ne vse, rusju ludis ili vo.",
+    "Tper posle kazahzem zxe es. Kai pervju cxesajut na. Mai mi onai rodzinuf cxesajut, mlodic libktor sos te. Ne klet suhju pridij gaz, na cxem zxiznuf razumju des. On gaz velm komnat. Dva dopis dolgju ti, gda bu goroh hrebet razumijut.Ne din glos dirkas igrajte, da tonkju starju germanju kak. Om oni goroh nozxis. Ona maluo tenis sipal es, tper zxiznuf mlodica tak na, telo zemla igrajte kai bu. Vse so dolgju naidil, din ne svadilju blagodarim, bez da imam zapalka. Pesna lubijsx razumju li tak. Divaj ribas do zxe.",
+    "Ul havi alial kurta cii, ts apude alternativdemando tuj. Duono grupa tempodaŭro plue ec, id per super jugoslavo. Dev du alternativa diskriminacio, ec iam liva duona, timi ometr supersigno en plu. Pre adjektiva antaŭelemento er, log so ikso inkluzive alikvante, dek e duto jesigi nombrovorto. Sis no geto speco interogativo. Ad tia lipa vatto centimetro, sin apostrofo kvintiliono gh. Vivui landonomo nun da, sia sekso sepen leterskribi bv.",
+    "Em decimalo altlernejo ali, ant volus tripunkto memkompreneble e, nek vira infra grado iz. Aga dz nevo tempolongo, oz kiu plus itismo scivolema. Nun neniu mallongigo miriametro zo, iom senobjekta komentofrazo um. Gv tio devi sekso. Nenii daralbajdo altmontaro kaj uk, trema bat'o senigi ec pri. El ioj vasta kernovorto esperantigita, konjunkcio postesigno nu nen. Ar siatempe ekskluzive tie, igi konateco respondeci daralbajdo nv, ar veo getto verbo.",
+    "Des ne unuj nomial akuzativa, u dolaro malpermesi ses. Drumo seksa dividostreko kv ism, aj tuja adjektiva mal, co multiplikite postpriskribo men. Gh ist plej tebi subjunkcio, ien kombi punktokomo rolvorteto hu, jes lumigi nomial kunmetita op. San ju longa definitive. Ind stif morgaŭa po, inkluzive landonomo os ene. Inter aliam elparolo ot pov, sor koruso dekuma vortfarado oz. Deci mallongigo nederlando via ba.",
+    "Vela semi vendo ti kia, numeralo centilitro kz tio, tre femto finaĵvorto pseŭdoafikso ju. Int ikso volus sekse ad. Latino depost cia hu. Poa unua ligfinaĵo em, i trema patro latina ari. Op mem viro ipsilono neŭtrala, ikso kvanto ili e, rekta lanta reciprokeco kuo co. Ed okej okcidente difiniteco aga, pri ajna pleje esceptinte ot, aha semajntago sekvinbero il.",
+    "Tote patre independente un del, ha tres texto auxiliar qui. Usate libere non lo, europa personas intermediari pro o. Un toto sanctificate del, comprende greco-latin concretisation ha sed. Mundial greco-latin ma pan, clave esser ascoltar sed de, major grammatica supervivite in duo. Technic vocabulario continentes nos ha, technic durante extrahite nos de. Le web veni parola, de commun technic occidental duo.",
+    "Technic instituto tentation web le, russo parolas auxiliar que su, non practic publication e. Duo publicate continentes ha, malo effortio initialmente al duo. Ma nos germano parolas. Sine europee uno e, historia extrahite qualcunque uno da. Super promotores le uno, unic regula publicava del o, tentation immediatemente es sed. Uso su nomina americano. O religion preparation secundarimente nos, pan pote encyclopedia ma. Maximo supervivite immediatemente il pan, sed ma veni libro. Russo cinque dictionario pan es. Via deler linguistas il, e infra publicate traducite nos. Per de tres scientia, se durante traducite con, studio voluntate lo del. Lo que regula svedese articulo. Nos ma malo medical romanic. Non in union membros lateres, nos anque association se. Sine existe interlinguistica non ma, patre latente se non. Sed infra effortio greco-latin se, non vista esseva spatios al. Altere sanctificate le sia, le terra pardona computator nos.",
+    "Quén tárë rangwë wen lá, car vá ninwa elendë. Talta nessa lingwë mí nún, vá nahamna hantalë caimassë ela. Ninwa maren turúva ma sar. Nu fárë collo ambarmetta wen. Yelma nahamna hón nu, fassë atacar artuilë lëo sí. Mer rata eques oa, isqua moina hérincë nu úil.",
+    "At limbë rámië arandur her, vi nóa fánë mardo fassë. Ambo orosta tellaurë oar ar, cen an sairon sindar, mac oi hyóla ettelen tengwanda. Hyóla atacar caimasan mí axa, simpetar tengwelë mí tuo. Tol né sáma anca voronwë, valda turma rempa pé low.",
+    "Mól at téra norna avamarwa. Aiwë rimpë lindelëa tar up, ar loa cala nimba raica, nár yávë nalda nu. Sanga nalda vi árë, ára to manu linga. Vén aráto nahamna ettelen or, nírë nortalarta yá íta, sië ep harë sáma. Lau nessa oaris nó, núta yúyo vórima mi pel. Sië cotumo lillassëa pé, hap pahta salmë palúrë cu."
+];
+
+var form = document.querySelector(".lorem-form");
+var amount = document.querySelector("#amount");
+var output = document.querySelector("#output");
+
+function formEventHandler(e) {
+    e.preventDefault();
+    const value = parseInt(amount.value); //reading the string value and converting it into an integer
+    const random = Math.floor(Math.random() * arr.length); //random function to generate random paragraphs at once
+
+    if (isNaN(value) || value < 0 || value > arr.length) { //for the following conditions NaN -> Not a Number
+        output.innerHTML = `<div class="output">${arr[random]}</div>` // it will display only one random paragraph
+    }
+    else {
+        let tempText = arr.slice(0, value);
+        tempText = tempText.map(function (item) {
+            return `<div class="output">${item}</div>`;
+        }).join("");
+        output.innerHTML = tempText;
+    }
+}
+
+if (form) {
+    form.addEventListener("submit", formEventHandler);
+}
