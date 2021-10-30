1c99c192d59cc523078baf3384fcb87d8733bc89
Zainab-Fahim <shafna.zainab.fahim@gmail.com>
2021-10-30 18:44:34 +0530

    added paint color buttons

--- Projects/Watercolor Paint/start.js
+++ Projects/Watercolor Paint/start.js
@@ -24,7 +24,7 @@
             const distance = getDistance(previousPoint, currentPoint)
             const opacity = Math.min(0.5, 1 / distance)
             context.lineWidth = Math.random() / distance * 40
-            context.strokeStyle = `rgba(222, 10, 109, ${opacity})`
+            context.strokeStyle = `rgba(${changeColor(document.querySelector(".active").id)},${opacity})`
 
             context.moveTo(previousPoint.x, previousPoint.y)
             context.lineTo(currentPoint.x, currentPoint.y)
@@ -51,3 +51,29 @@
     }
     run()
 })();
+
+const button = document.querySelectorAll(".button");
+const selectColor = (elem) => {
+    removeActiveCircleColor();
+    elem.classList.add("active");
+};
+  
+const removeActiveCircleColor = () => {
+    button.forEach((circle) => {
+      circle.classList.remove("active");
+    });
+};
+
+function changeColor(color){
+    if (color=='red'){
+        return '255,0,0';
+    }else if(color=='blue'){
+        return '0,0,255';
+    }else if(color=='green'){
+        return '0,255,0';
+    }else if(color=='gray'){
+        return '100,100,100';
+    }else if(color=='black'){
+        return '0,0,0';
+    }
+}
